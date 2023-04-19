3e5c42964668971a8d712b41b0b89d988b8b585c
kxxhyorim <rlagyfla0709@gmail.com>
2023-04-19 22:56:11 +0900

    [UI] BottomNavigationView 추가

--- app/src/main/java/org/swm/mammamia/ui/MainActivity.kt
+++ app/src/main/java/org/swm/mammamia/ui/MainActivity.kt
@@ -1,10 +1,11 @@
 package org.swm.mammamia.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import org.swm.mammamia.R
+import org.swm.mammamia.databinding.ActivityMainBinding
+import org.swm.mammamia.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
